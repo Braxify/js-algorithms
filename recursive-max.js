6a5435d7516e11f8f2f782a8ef2242a40186f007
Braxify <art.nazarian1@gmail.com>
2023-06-30 21:06:21 +0300

    add the recursive script

---
+++ recursive-max.js
@@ -0,0 +1,9 @@
+function recursiveMax(arr, max = 0) {
+    if (!arr.length) {
+        return max;
+    }
+    return recursiveMax(arr.slice(1), arr[0] > max ? arr[0] : max);
+}
+
+const a = [1, 2, 3, 4, 9, 5];
+console.log(recursiveMax(a));
