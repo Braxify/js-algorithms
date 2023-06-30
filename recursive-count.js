7b017c6a50c5b17cc41c4a3d27f98746d2da2dc1
Braxify <art.nazarian1@gmail.com>
2023-06-30 20:53:53 +0300

    Revert "add the recursive scripts"
    This reverts commit a2380171168b838bca8fc3dd8df31e0a7f7a8538.

--- recursive-count.js
+++
@@ -1,9 +0,0 @@
-function recursiveCount(arr) {
-    if (!arr.length) {
-        return 0;
-    }
-    return 1 + recursiveCount(arr.slice(1));
-}
-
-const a = [1, 2, 3, 4, 5];
-console.log(recursiveCount(a));
--- recursive-sum.js
+++
@@ -1,9 +0,0 @@
-function recursiveSum(arr) {
-    if (!arr.length) {
-        return 0;
-    }
-    return arr[0] + recursiveSum(arr.slice(1));
-}
-
-const a = [1, 2, 3, 4, 5];
-console.log(recursiveSum(a));
