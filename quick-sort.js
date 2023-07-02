1ee03a4673e5abea638ab36e60a6ba68fbe319d8
Braxify <art.nazarian1@gmail.com>
2023-07-02 21:12:59 +0300

    add new example

--- quick-sort.js
+++ quick-sort.js
@@ -8,5 +8,26 @@ function quickSort(arr) {
     return quickSort(lessThanPivot).concat([pivot], quickSort(greaterThanPivot));
 }
 
-const a = [1, 8, 5, 9];
+function quickSort2(arr) {
+    if (arr.length < 2) return arr;
+
+    const pivot = arr[0];
+    const lessThanPivot = [];
+    const greaterThanPivot = [];
+    arr.forEach((el, idx) => {
+        if (idx === 0) {
+            return;
+        }
+        if (el <= pivot) {
+            lessThanPivot.push(el);
+        } else if (el > pivot) {
+            greaterThanPivot.push(el);
+        }
+    });
+
+    return quickSort(lessThanPivot).concat([pivot], quickSort(greaterThanPivot));
+}
+
+const a = [9, 8, 1, 5];
 console.log(quickSort(a));
+console.log(quickSort2(a));
