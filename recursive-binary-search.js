90f23494008e8640e97a9e2ddf6f861ae423d834
Braxify <art.nazarian1@gmail.com>
2023-07-05 22:32:28 +0300

    add the recursive binary search

---
+++ recursive-binary-search.js
@@ -0,0 +1,21 @@
+function binarySearch(array, item, low = 0, high = array.length - 1) {
+  if (low > high) {
+    return null;
+  }
+
+  let mid = Math.floor((low + high) / 2);
+  let guess = array[mid];
+
+  if (guess === item) {
+    return mid;
+  }
+
+  if (guess > item) {
+    return binarySearch(array, item, low, mid - 1);
+  } else {
+    return binarySearch(array, item, mid + 1, high);
+  }
+}
+
+const arr = [1, 2, 3, 5, 4];
+console.log(binarySearch(arr, 2));
