5155fc2d50db37210629fde97cdf5d919f120c7e
Arthur <art.nazarian1@gmail.com>
2023-08-13 22:05:15 +0300

    add the example for recursive-max

--- recursive-max.js
+++ recursive-max.js
@@ -5,5 +5,12 @@ function recursiveMax(arr, max = 0) {
     return recursiveMax(arr.slice(1), arr[0] > max ? arr[0] : max);
 }
 
+function recursiveMax2([first, ...rest], max = 0) {
+    return first === undefined ? max : recursiveMax(rest, first > max ? first : max);
+}
+
 const a = [1, 2, 3, 4, 9, 5, -5];
 console.log(recursiveMax(a));
+
+const b = [1, 2, 3, 4, 9, 5, -5];
+console.log(recursiveMax2(b));
