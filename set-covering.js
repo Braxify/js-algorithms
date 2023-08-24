93f5ec7d7e7a24de636be070b10ad514c3bfa44c
Arthur <art.nazarian1@gmail.com>
2023-08-24 16:42:37 +0300

    upd the comment

--- set-covering.js
+++ set-covering.js
@@ -1,7 +1,9 @@
 let statesNeeded = new Set(["mt", "wa", "or", "id", "nv", "ut", "ca", "az"]);
 
-// Хеш-таблица станций, из которой будет выбираться покрытие.
-// Ключи - названия станций, а значения - сокращенные обозначения штатов, входящих в зону охвата.
+/* 
+  Хеш-таблица станций, из которой будет выбираться покрытие.
+  Ключи - названия станций, а значения - сокращенные обозначения штатов, входящих в зону охвата.
+*/
 const stations = {};
 stations.kone = new Set(["id", "nv", "ut"]);
 stations.ktwo = new Set(["wa", "id", "mt"]);
@@ -14,16 +16,18 @@ const finalStations = new Set();
 while (statesNeeded.size) {
   let bestStation = null;
   let statesCovered = new Set();
-  
-  Object.keys(stations).forEach(station => {
+
+  Object.keys(stations).forEach((station) => {
     const states = stations[station];
-    const covered = new Set([...statesNeeded].filter(x => states.has(x)));
+    const covered = new Set([...statesNeeded].filter((x) => states.has(x)));
     if (covered.size > statesCovered.size) {
       bestStation = station;
       statesCovered = covered;
     }
   });
-  statesNeeded = new Set([...statesNeeded].filter(x => !statesCovered.has(x)));
+  statesNeeded = new Set(
+    [...statesNeeded].filter((x) => !statesCovered.has(x))
+  );
   finalStations.add(bestStation);
 }
 
